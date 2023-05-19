000f55a94c75d19dcca35fb6bf2c3de6f69b7869
felipedossantosoliveira <felipedossantosads@gmail.com>
2023-05-19 11:45:15 -0300

    style: show examples

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,60 @@
 <script setup>
-import MultiLineChartExample from './examples/MultiLineChartExample.vue';
-import {computed, ref} from "vue";
+import MultiLineChartExample from "./examples/MultiLineChartExample.vue";
+import { computed, ref } from "vue";
 import LineChartExample from "./examples/LineChartExample.vue";
 import BarsChartExample from "./examples/BarsChartExample.vue";
 
-const theme = ref('dark')
+const theme = ref("dark");
 
-function switchTheme(){
-  theme.value = theme.value === 'dark' ? 'light' : 'dark'
+function switchTheme() {
+  theme.value = theme.value === "dark" ? "light" : "dark";
 }
 
 const backgroundColor = computed(() => {
-  return theme.value === 'dark' ? '#0f172a' : '#fff'
-})
-
+  return theme.value === "dark" ? "#0f172a" : "#fff";
+});
 </script>
 
 <template>
-  <div class="body" :style="{'--color': backgroundColor}">
-  <button class="button" @click="switchTheme()">
-    toggle
-  </button>
-<!--  <MultiLineChartExample/>-->
-<!--    <LineChartExample/>-->
-    <BarsChartExample/>
-
+  <div class="body" :style="{ '--color': backgroundColor }">
+    <button class="button" @click="switchTheme()">toggle</button>
+    <BarsChartExample />
+    <div
+      style="
+         {
+          display: grid;
+          grid-template-columns: repeat(2, 1fr);
+        }
+      "
+    >
+      <div :style="{
+        display: 'flex',
+      }">
+      <div
+        :style="{
+          width: '50%',
+          height: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }"
+      >
+        <MultiLineChartExample />
+      </div>
+      <div
+        :style="{
+          width: '50%',
+          height: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }"
+      >
+        <LineChartExample />
+      </div>
+      </div>
+    </div>
   </div>
-
 </template>
 <style>
-
 .button {
   margin-bottom: 100px;
   border: 0;
@@ -38,11 +63,10 @@ const backgroundColor = computed(() => {
 }
 
 .body {
-  width: 100vw ;
-  height: 5000px ;
+  width: 100vw;
+  height: 5000px;
   margin-left: -7px;
   margin-top: -8px;
   background-color: var(--color);
 }
-
 </style>
