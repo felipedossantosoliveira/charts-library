bd8ff8a734a3cceb91c70c6474adce5c789692fe
felipedossantosoliveira <felipedossantosads@gmail.com>
2023-05-29 16:51:03 -0300

    style: prettier

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,202 +1,202 @@
 <script setup>
-import {reactive, computed, onMounted, onUnmounted, ref} from "vue";
+import { reactive, computed, onMounted, onUnmounted, ref } from "vue";
 
 const props = defineProps({
   color: {
     type: String,
     required: false,
-    default: 'red'
+    default: "red",
   },
   data: {
     type: Array,
-    required: true
+    required: true,
   },
   theme: {
     type: String,
     required: false,
     default: "light",
   },
-})
+});
 
 const colorHex = computed(() => {
-  switch (props.color){
-    case 'amber':
+  switch (props.color) {
+    case "amber":
       return {
-        op200: '#fde68a',
-        op300: '#fcd34d',
-        op500: '#f59e0b'
+        op200: "#fde68a",
+        op300: "#fcd34d",
+        op500: "#f59e0b",
       };
-    case 'blue':
+    case "blue":
       return {
-        op200: '#bfdbff',
-        op300: '#93c5fd',
-        op500: '#3b82f6'
+        op200: "#bfdbff",
+        op300: "#93c5fd",
+        op500: "#3b82f6",
       };
-    case 'cyan':
+    case "cyan":
       return {
-        op200: '#a5f3fc',
-        op300: '#67e8f9',
-        op500: '#06b6d4'
+        op200: "#a5f3fc",
+        op300: "#67e8f9",
+        op500: "#06b6d4",
       };
-    case 'emerald':
+    case "emerald":
       return {
-        op200: '#a7f3d0',
-        op300: '#6ee7b7',
-        op500: '#10b981'
+        op200: "#a7f3d0",
+        op300: "#6ee7b7",
+        op500: "#10b981",
       };
-    case 'fuchsia':
+    case "fuchsia":
       return {
-        op200: '#f5d0fe',
-        op300: '#f0abfc',
-        op500: '#d946ef'
+        op200: "#f5d0fe",
+        op300: "#f0abfc",
+        op500: "#d946ef",
       };
-    case 'gray':
+    case "gray":
       return {
-        op200: '#e5e7eb',
-        op300: '#d1d5db',
-        op500: '#6b7280'
+        op200: "#e5e7eb",
+        op300: "#d1d5db",
+        op500: "#6b7280",
       };
-    case 'green':
+    case "green":
       return {
-        op200: '#bbf7d0',
-        op300: '#86efac',
-        op500: '#22c55e'
+        op200: "#bbf7d0",
+        op300: "#86efac",
+        op500: "#22c55e",
       };
-    case 'indigo':
+    case "indigo":
       return {
-        op200: '#c7d2fe',
-        op300: '#a5b4fc',
-        op500: '#6366f1'
+        op200: "#c7d2fe",
+        op300: "#a5b4fc",
+        op500: "#6366f1",
       };
-    case 'lime':
+    case "lime":
       return {
-        op200: '#d9f99d',
-        op300: '#bee264',
-        op500: '#84cc16'
+        op200: "#d9f99d",
+        op300: "#bee264",
+        op500: "#84cc16",
       };
-    case 'neutral':
+    case "neutral":
       return {
-        op200: '#e5e5e5',
-        op300: '#d4d4d4',
-        op500: '#737373'
+        op200: "#e5e5e5",
+        op300: "#d4d4d4",
+        op500: "#737373",
       };
-    case 'orange':
+    case "orange":
       return {
-        op200: '#fed7aa',
-        op300: '#fdba74',
-        op500: '#f97316'
+        op200: "#fed7aa",
+        op300: "#fdba74",
+        op500: "#f97316",
       };
-    case 'pink':
+    case "pink":
       return {
-        op200: '#fbcfe8',
-        op300: '#f9a8d4',
-        op500: '#ec4899'
+        op200: "#fbcfe8",
+        op300: "#f9a8d4",
+        op500: "#ec4899",
       };
-    case 'purple':
+    case "purple":
       return {
-        op200: '#e9d5ff',
-        op300: '#d8b4fe',
-        op500: '#a855f7'
+        op200: "#e9d5ff",
+        op300: "#d8b4fe",
+        op500: "#a855f7",
       };
-    case 'red':
+    case "red":
       return {
-        op200: '#fecaca',
-        op300: '#fca5a5',
-        op500: '#ef4444'
+        op200: "#fecaca",
+        op300: "#fca5a5",
+        op500: "#ef4444",
       };
-    case 'rose':
+    case "rose":
       return {
-        op200: '#fecdd3',
-        op300: '#fda4af',
-        op500: '#f43f5e'
+        op200: "#fecdd3",
+        op300: "#fda4af",
+        op500: "#f43f5e",
       };
-    case 'sky':
+    case "sky":
       return {
-        op200: '#bae6fd',
-        op300: '#7dd3fc',
-        op500: '#0ea5e9'
+        op200: "#bae6fd",
+        op300: "#7dd3fc",
+        op500: "#0ea5e9",
       };
-    case 'slate':
+    case "slate":
       return {
-        op200: '#e2e8f0',
-        op300: '#cbd5e1',
-        op500: '#64748b'
+        op200: "#e2e8f0",
+        op300: "#cbd5e1",
+        op500: "#64748b",
       };
-    case 'stone':
+    case "stone":
       return {
-        op200: '#e7e5e4',
-        op300: '#d6d3d1',
-        op500: '#78716c'
+        op200: "#e7e5e4",
+        op300: "#d6d3d1",
+        op500: "#78716c",
       };
-    case 'teal':
+    case "teal":
       return {
-        op200: '#99f6e4',
-        op300: '#5eead4',
-        op500: '#14b8a6'
+        op200: "#99f6e4",
+        op300: "#5eead4",
+        op500: "#14b8a6",
       };
-    case 'violet':
+    case "violet":
       return {
-        op200: '#ddd6fe',
-        op300: '#c4b5fd',
-        op500: '#8b5cf6'
+        op200: "#ddd6fe",
+        op300: "#c4b5fd",
+        op500: "#8b5cf6",
       };
-    case 'yellow':
+    case "yellow":
       return {
-        op200: '#fef08a',
-        op300: '#fde047',
-        op500: '#e6b308'
+        op200: "#fef08a",
+        op300: "#fde047",
+        op500: "#e6b308",
       };
-    case 'zinc':
+    case "zinc":
       return {
-        op200: '#e4e4e7',
-        op300: '#d4d4d8',
-        op500: '#717179'
+        op200: "#e4e4e7",
+        op300: "#d4d4d8",
+        op500: "#717179",
       };
     default:
       return {
-        op200: '#fed7aa',
-        op300: '#fdba74',
-        op500: '#f97316',
+        op200: "#fed7aa",
+        op300: "#fdba74",
+        op500: "#f97316",
       };
   }
-})
+});
 
-const x = ref(0)
-const y = ref(0)
+const x = ref(0);
+const y = ref(0);
 
 function update(event) {
-  x.value = event.pageX
-  y.value = event.pageY
+  x.value = event.pageX;
+  y.value = event.pageY;
 }
 
-onMounted(() => window.addEventListener('mousemove', update))
-onUnmounted(() => window.removeEventListener('mousemove', update))
+onMounted(() => window.addEventListener("mousemove", update));
+onUnmounted(() => window.removeEventListener("mousemove", update));
 
 //Tooltip setup
-function showTooltip(key, value, index){
-  show.value = true
-  show.data.key = key
-  show.data.value = value.toFixed(4)
-  show.data.index = index
+function showTooltip(key, value, index) {
+  show.value = true;
+  show.data.key = key;
+  show.data.value = value.toFixed(4);
+  show.data.index = index;
 }
 
-function hideTooltip(){
-  show.value = false
-  show.data.key = ''
-  show.data.value = ''
-  show.data.index = null
+function hideTooltip() {
+  show.value = false;
+  show.data.key = "";
+  show.data.value = "";
+  show.data.index = null;
 }
 
 const show = reactive({
   value: false,
   data: {
-    key: '',
+    key: "",
     value: 0,
-    index: null
-  }
-})
+    index: null,
+  },
+});
 
-const items = props.data
+const items = props.data;
 
 //Chart setup
 const maxItems = computed(() => {
@@ -210,7 +210,7 @@ const minItems = computed(() => {
 function calculatePercentageInverted(value) {
   let val = value - minItems.value;
   let max = maxItems.value - minItems.value;
-  let result = (100 - ((val * 100) / max))+30;
+  let result = 100 - (val * 100) / max + 30;
   return result.toFixed(2);
 }
 
@@ -219,79 +219,106 @@ const interval = computed(() => {
 });
 
 const points = computed(() => {
-  let points = []
-  for (let i=0; i < items.length; i++){
-    points.push((i*40)+40);
-    points.push(calculatePercentageInverted(items[i].value))
+  let points = [];
+  for (let i = 0; i < items.length; i++) {
+    points.push(i * 40 + 40);
+    points.push(calculatePercentageInverted(items[i].value));
   }
-  return points.join(' ')
+  return points.join(" ");
 });
 
 const pointsAnimate = computed(() => {
-  let points = []
-  for (let i=0; i < items.length; i++){
-    points.push((i*40)+40);
-    points.push(80)
+  let points = [];
+  for (let i = 0; i < items.length; i++) {
+    points.push(i * 40 + 40);
+    points.push(80);
   }
-  return points.join(' ')
+  return points.join(" ");
 });
 
 const pointsPolygon = computed(() => {
-  let points = [40, 130]
-  for (let i=0; i < items.length; i++){
-    points.push((i*40)+40);
-    points.push(calculatePercentageInverted(items[i].value))
+  let points = [40, 130];
+  for (let i = 0; i < items.length; i++) {
+    points.push(i * 40 + 40);
+    points.push(calculatePercentageInverted(items[i].value));
   }
-  let final = items.length * 40
+  let final = items.length * 40;
   points.push(final, 130);
-  return points.join(' ')
+  return points.join(" ");
 });
 
 const pointsPolygonAnimate = computed(() => {
-  let points = [40, 130]
-  for (let i=0; i < items.length; i++){
-    points.push((i*40)+40);
-    points.push(80)
+  let points = [40, 130];
+  for (let i = 0; i < items.length; i++) {
+    points.push(i * 40 + 40);
+    points.push(80);
   }
-  let final = items.length * 40
+  let final = items.length * 40;
   points.push(final, 130);
-  return points.join(' ')
+  return points.join(" ");
 });
 
 const viewbox = computed(() => {
-  return `0 0 ${items.length * 40 + 30} 150`
+  return `0 0 ${items.length * 40 + 30} 150`;
 });
 
 const horizontalLinesWidth = computed(() => {
-  return items.length * 40
+  return items.length * 40;
 });
 </script>
 <template>
-  <div :style="{
+  <div
+    :style="{
       '--color': colorHex.op500,
       '--color-200': colorHex.op200,
       '--color-300': colorHex.op300,
     }"
   >
-    <svg
-      class="svg"
-      :viewBox="viewbox"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg class="svg" :viewBox="viewbox" xmlns="http://www.w3.org/2000/svg">
       <!--   Valores das divisões horizontais || Values of horizontal divisions   -->
-      <text :style="'fill: ' + (props.theme === 'dark' ? '#ffffff' : '#000000')" x="35" y="30" text-anchor="end" class="horizontal_text">
+      <text
+        :style="'fill: ' + (props.theme === 'dark' ? '#ffffff' : '#000000')"
+        x="35"
+        y="30"
+        text-anchor="end"
+        class="horizontal_text"
+      >
         {{ maxItems }}
       </text>
-      <text :style="'fill: ' + (props.theme === 'dark' ? '#ffffff' : '#000000')" x="35" y="55" text-anchor="end" class="horizontal_text">
+      <text
+        :style="'fill: ' + (props.theme === 'dark' ? '#ffffff' : '#000000')"
+        x="35"
+        y="55"
+        text-anchor="end"
+        class="horizontal_text"
+      >
         {{ (maxItems - interval).toFixed(0) }}
       </text>
-      <text :style="'fill: ' + (props.theme === 'dark' ? '#ffffff' : '#000000')" x="35" y="80" text-anchor="end" class="horizontal_text">
+      <text
+        :style="'fill: ' + (props.theme === 'dark' ? '#ffffff' : '#000000')"
+        x="35"
+        y="80"
+        text-anchor="end"
+        class="horizontal_text"
+      >
         {{ (maxItems - interval * 2).toFixed(0) }}
       </text>
-      <text :style="'fill: ' + (props.theme === 'dark' ? '#ffffff' : '#000000')" x="35" y="105" text-anchor="end" class="horizontal_text">
+      <text
+        :style="'fill: ' + (props.theme === 'dark' ? '#ffffff' : '#000000')"
+        x="35"
+        y="105"
+        text-anchor="end"
+        class="horizontal_text"
+      >
         {{ (maxItems - interval * 3).toFixed(0) }}
       </text>
-      <text :style="'fill: ' + (props.theme === 'dark' ? '#ffffff' : '#000000')" x="35" y="130" text-anchor="end" class="horizontal_text">
+      <text
+        :style="'fill: ' + (props.theme === 'dark' ? '#ffffff' : '#000000')"
+        x="35"
+        y="130"
+        text-anchor="end"
+        class="horizontal_text"
+      >
         {{ minItems }}
       </text>
 
@@ -344,7 +371,7 @@ const horizontalLinesWidth = computed(() => {
 
       <!--   Valores das divisões verticais || Values of vertical divisions   -->
       <text
-          :style="'fill: ' + (props.theme === 'dark' ? '#ffffff' : '#000000')"
+        :style="'fill: ' + (props.theme === 'dark' ? '#ffffff' : '#000000')"
         v-for="(key, index) in items"
         :key="index"
         :x="index * 40 + 40"
@@ -376,7 +403,13 @@ const horizontalLinesWidth = computed(() => {
         stroke-width="0"
         stroke-linejoin="round"
       >
-        <animate attributeName="points" dur="0.3s" repeatCount="1" :values="pointsPolygonAnimate + ';' + pointsPolygon" keyTimes="0; 1" />
+        <animate
+          attributeName="points"
+          dur="0.3s"
+          repeatCount="1"
+          :values="pointsPolygonAnimate + ';' + pointsPolygon"
+          keyTimes="0; 1"
+        />
       </polygon>
       <polyline
         stroke="currentColor"
@@ -386,7 +419,13 @@ const horizontalLinesWidth = computed(() => {
         fill="none"
         stroke-linejoin="round"
       >
-        <animate attributeName="points" dur="0.3s" repeatCount="1" :values="pointsAnimate + ';' + points" keyTimes="0; 1" />
+        <animate
+          attributeName="points"
+          dur="0.3s"
+          repeatCount="1"
+          :values="pointsAnimate + ';' + points"
+          keyTimes="0; 1"
+        />
       </polyline>
 
       <!--   Pontos do gráfico || Points of chart    -->
@@ -398,7 +437,13 @@ const horizontalLinesWidth = computed(() => {
         r="2.5"
         fill="currentColor"
       >
-      <animate attributeName="cy" dur="0.3s" repeatCount="1" :values="'80' + ';' + calculatePercentageInverted(item.value)" keyTimes="0; 1" />
+        <animate
+          attributeName="cy"
+          dur="0.3s"
+          repeatCount="1"
+          :values="'80' + ';' + calculatePercentageInverted(item.value)"
+          keyTimes="0; 1"
+        />
       </circle>
 
       <circle
@@ -411,14 +456,20 @@ const horizontalLinesWidth = computed(() => {
         @mouseenter="showTooltip(items[index].key, item.value, index)"
         @mouseleave="hideTooltip"
       >
-        <animate attributeName="cy" dur="0.3s" repeatCount="1" :values="'80' + ';' + calculatePercentageInverted(item.value)" keyTimes="0;1" />
+        <animate
+          attributeName="cy"
+          dur="0.3s"
+          repeatCount="1"
+          :values="'80' + ';' + calculatePercentageInverted(item.value)"
+          keyTimes="0;1"
+        />
       </circle>
     </svg>
     <div
       :style="{
         '--x': (show.data.index + 1 > items.length / 2 ? x - 150 : x) + 'px',
         '--y': y - 75 + 'px',
-        '--color-200-20': colorHex.op300 + '70'
+        '--color-200-20': colorHex.op300 + '70',
       }"
       class="tooltip"
       v-show="show.value"
@@ -434,62 +485,61 @@ const horizontalLinesWidth = computed(() => {
 </template>
 <style scoped>
 .tooltip {
-	position: absolute;
-	top: var(--y);
-	left: var(--x);
-	width: 150px;
-	z-index: 100;
-	overflow-y: hidden;
-	border-radius: 0.5rem;
-	background-color: var(--color-200-20);
-	backdrop-filter: blur(10px);
-	-webkit-backdrop-filter: blur(10px);
-  }
-  
-  .horizontal_text {
-	font-size: 9pt;
-  }
-  
-  .svg {
-	color: var(--color);
-  }
-  
-  .line_color {
-	color: #a3a3a3;
-  }
-  
-  svg text {
-	fill: #000;
-	font-size: 9pt;
-  }
-  
-  .polygon {
-	fill: var(--color-300);
-	fill-opacity: 0.2;
-  }
-  
-  .circle {
-	fill-opacity: 0;
-	fill: var(--color);
-  }
-  
-  .circle:hover {
-	fill-opacity: 0.2;
-  }
-  
-  .tooltip-atribute {
-	overflow: hidden;
-	text-overflow: ellipsis;
-	white-space: nowrap;
-	border-left-width: 8px;
-  border-left-style: solid;
-	border-color: var(--color);
-	padding: 0.25rem 0.75rem;
-  }
-  
-  .tooltip-key {
-	border-bottom-width: 1px;
-	border-bottom-style: solid;
-  }
-</style>
+  position: absolute;
+  top: var(--y);
+  left: var(--x);
+  width: 150px;
+  z-index: 100;
+  overflow-y: hidden;
+  border-radius: 0.5rem;
+  background-color: var(--color-200-20);
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+}
 
+.horizontal_text {
+  font-size: 9pt;
+}
+
+.svg {
+  color: var(--color);
+}
+
+.line_color {
+  color: #a3a3a3;
+}
+
+svg text {
+  fill: #000;
+  font-size: 9pt;
+}
+
+.polygon {
+  fill: var(--color-300);
+  fill-opacity: 0.2;
+}
+
+.circle {
+  fill-opacity: 0;
+  fill: var(--color);
+}
+
+.circle:hover {
+  fill-opacity: 0.2;
+}
+
+.tooltip-atribute {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  border-left-width: 8px;
+  border-left-style: solid;
+  border-color: var(--color);
+  padding: 0.25rem 0.75rem;
+}
+
+.tooltip-key {
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+}
+</style>
